cd0110f01876e12392b35bbf2c1ce84e5b64e67b
Sebastian Rykowski Zeerak <sebastian.zeerak@gmail.com>
2025-10-26 15:50:19 +0100

    2024 day03: second star completed

--- advent-of-code-2024/day03/memory.py
+++ advent-of-code-2024/day03/memory.py
@@ -1,20 +1,49 @@
 import re
+from enum import StrEnum
 
 
-def unscramble_memory(text: str) -> int:
+class Mode(StrEnum):
+    DO = "DO"
+    DO_NOT = "DO_NOT"
+
+
+def unscramble_memory(text: str, use_mode: bool = False) -> int:
     mul_instructions = _get_mul_instructions(text)
 
-    return mul_instructions
+    calculations = _calculate(mul_instructions, use_mode=use_mode)
+    return calculations
 
 
-def _get_mul_instructions(text: str) -> int:
-    regex = r"mul\((\d+),(\d+)\)"
-    result = 0
+def _get_mul_instructions(text: str) -> list[str]:
+    regex = r"mul\(\d+,\d+\)|do\(\)|don't\(\)"
+    result = []
     for line in text.splitlines():
         line = line.strip()
         if not line:
             continue
         matches = re.findall(regex, line)
-        for match in matches:
-            result += (int(match[0]) * int(match[1]))
+        result.extend(matches)
     return result
+
+
+def _calculate(instructions: list[str], use_mode: bool = False) -> int:
+    regex = r"\d+"
+    current_mode = Mode.DO
+
+    result = 0
+    for instruction in instructions:
+        if instruction.startswith("mul"):
+            if _should_add(use_mode, current_mode):
+                matches = re.findall(regex, instruction)
+                result += int(matches[0]) * int(matches[1])
+        elif instruction == "don't()":
+            current_mode = Mode.DO_NOT
+        elif instruction == "do()":
+            current_mode = Mode.DO
+    return result
+
+
+def _should_add(use_mode: bool, mode: Mode) -> bool:
+    if not use_mode:
+        return True
+    return mode == Mode.DO
--- advent-of-code-2024/day03/memory_test.py
+++ advent-of-code-2024/day03/memory_test.py
@@ -5,6 +5,10 @@ EXAMPLE = """
 xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
 """.strip()
 
+EXAMPLE2 = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+""".strip()
+
 
 def test_unscramble_memory_example():
     actual = unscramble_memory(EXAMPLE)
@@ -14,6 +18,16 @@ def test_unscramble_memory_example():
 def test_safe_reports():
     actual = unscramble_memory(_read_file("input"))
     assert actual == 174103751
+
+
+def test_safe_reports_with_more_instructions_example():
+    actual = unscramble_memory(EXAMPLE2, use_mode=True)
+    assert actual == 48
+
+
+def test_safe_reports_with_more_instructions():
+    actual = unscramble_memory(_read_file("input"), use_mode=True)
+    assert actual == 100411201
 
 
 def _read_file(filename: str):
